17f58c260a103e0bc3e5abaf164ce741f4c4c940
Soumya Ray <soumya.ray@gmail.com>
2014-05-28 23:16:27 +0800

    basic spec written (failing)

---
+++ spec/citesight_spec.rb
@@ -0,0 +1,19 @@
+require 'minitest/autorun'
+require 'minitest/rg'
+require './spec/minitest_helper.rb'
+
+describe 'Paper', 'A text document' do
+
+  describe 'when the paper exists' do
+
+    before do
+      contents = File.open('./spec/testfiles/test.txt', 'r').read
+      @results = PaperCitations.unique_cites(contents).sort_by { |c, _| c.downcase }
+    end
+
+    it 'has found the right citations' do
+      @results.must_equal TEST_TXT_RESULTS
+    end
+
+  end
+end
---
+++ spec/minitest_helper.rb
@@ -0,0 +1,19 @@
+require './lib/citesight.rb'
+
+TEST_TXT_RESULTS =
+  {
+    "Charlton 2002"=>1,
+    "De Boor 1980"=>1,
+    "Griffins 2000"=>1,
+    "Griffiths 2000"=>1,
+    "Hur 2007"=>2,
+    "Ma and Agarwal 2007"=>1,
+    "Ma et al. 2002"=>1,
+    "McDonald 2003"=>1,
+    "O'Hern 2010"=>1,
+    "Oreg 1995b"=>1,
+    "Peters et al. 2007"=>2,
+    "Ray 2000"=>1,
+    "van der Aalst 2004"=>1,
+    "Wa'el et al. 1993"=>1
+  }
