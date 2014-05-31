6c0ebd109a850270024ff643010054c484fb2a6e
Soumya Ray <soumya.ray@gmail.com>
2014-05-31 11:10:22 +0800

    better documentaion for index_cite; code cleanup; start new specs

--- spec/citesight_spec.rb
+++ spec/citesight_spec.rb
@@ -4,23 +4,29 @@ require './spec/minitest_helper.rb'
 
 describe 'Paper', 'A text document' do
 
-  describe 'when the paper exists' do
+  describe 'when there are citations in the text' do
     before do
       @results = PaperCitations.unique_cites(TEST_CONTENTS)
     end
 
-    it 'has found the right citations' do
+    it 'should fine the right citations' do
       @results.must_equal TEST_TXT_RESULTS
     end
   end
 
-
+  describe 'when there are no citations to be found' do
+    it 'should return an empty hash' do
+      no_cites_txt = 'these are not the citations you are looking for'
+      # TODO: check return of empty hash
+    end
+  end
 end
 
 describe 'Cases', 'Test different citation cases' do
   TEST_CASES.keys.each do |k|
-    it "#{k}" do
-      PaperCitations.unique_cites(TEST_CASES[k]["case"]).to_a.must_equal(TEST_CASES[k]["result"])
+    it "can detect #{k}" do
+      PaperCitations.unique_cites(TEST_CASES[k]["case"]).to_a\
+        .must_equal(TEST_CASES[k]["result"])
     end
   end
 
